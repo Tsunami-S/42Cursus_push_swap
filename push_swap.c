015c5978bff3aea562365e77e9c4f75c65b2d59d
TsunamiSaito <jvlipnitsk@gmail.com>
2024-12-20 00:54:33 +0900

    now

--- push_swap.c
+++ push_swap.c
@@ -1,32 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tssaito <tssaito@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 00:54:21 by tssaito           #+#    #+#             */
+/*   Updated: 2024/12/20 00:54:23 by tssaito          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void push_swap(t_list *list)
+static int	is_sorted(t_list *list);
+
+void	push_swap(t_list *list)
 {
-	while(ft_issorted(list) == ERROR)
+	while (ft_issorted(list) == ERROR)
 	{
-		if()
-			sa
-		else if()
-			sb
-		else if()
-			ss
-		else if()
-			pa
-		else if()
-			pb
-		else if()
-			pp
-		else if()
-			ra
-		else if()
-			rb
-		else if()
-			rr
-		else if()
-			rra
-		else if()
-			rrb
-		else if()
-			rrr
-		}
+		if ()
+			sa(list);
+		else if ()
+			sb(list);
+		else if ()
+			ss(list);
+		else if ()
+			pa(list);
+		else if ()
+			pb(list);
+		else if ()
+			ra(list);
+		else if ()
+			rb(list);
+		else if ()
+			rr(list);
+		else if ()
+			rra(list);
+		else if ()
+			rrb(list);
+		else if ()
+			rrr(list);
+	}
+}
+
+static int	is_sorted(t_list *list)
+{
+	t_stack	*now;
+
+	if (list->start_b)
+		return (ERROR);
+	now = list->start_a;
+	while (now->next)
+	{
+		if (now->n >= now->next - n)
+			return (ERROR);
+		now = now->next;
+	}
+	return (SUCCESS);
 }
