685005bc3acee3cc45c5ade759e82f78ca3d313c
TsunamiSaito <jvlipnitsk@gmail.com>
2025-01-06 00:35:55 +0900

    now

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tssaito <tssaito@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 00:18:03 by tssaito           #+#    #+#             */
-/*   Updated: 2025/01/06 00:26:24 by tssaito          ###   ########.fr       */
+/*   Updated: 2025/01/06 00:35:50 by tssaito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,29 +66,24 @@ void				sort_small_stack(t_list *list, int count);
 void				sort_small_a_bottom(t_list *list, int count);
 void				sort_small_b_top(t_list *list, int count);
 void				sort_small_b_bottom(t_list *list, int count);
-
-void				compress_cmds(t_list *list, t_act act);
-void				consecutive_cmds(t_list *list, t_act act, int count);
-
-int					issorted_a(t_list *list);
-int					issorted_go_next(t_stack *start, int count);
-int					issorted_go_prev(t_stack *start, int count);
-
+/* sort_utils */
+int					stack_count(t_stack *stack);
 void				make_pivot_go_next(t_stack *start, int size, int *large,
 						int *small);
 void				make_pivot_go_prev(t_stack *start, int size, int *large,
 						int *small);
-
-/* sort_utils */
-int					stack_count(t_stack *stack);
-
+int					issorted_a(t_list *list);
+int					issorted_go_next(t_stack *start, int count);
+int					issorted_go_prev(t_stack *start, int count);
 /* list_utils */
 void				error(void);
 void				list_init(t_list *list);
 void				delete_stack(t_list *list);
 int					init_stack_from_one(char *str, t_list *list);
 int					init_stack_from_args(int argc, char **argv, t_list *list);
-
+/* compress_cmds */
+void				compress_cmds(t_list *list, t_act act);
+void				consecutive_cmds(t_list *list, t_act act, int count);
 /* comannds */
 void				swap(t_list *list, t_act act);
 void				push(t_list *list, t_act act);
